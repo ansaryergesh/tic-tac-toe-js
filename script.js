d311ea975c86be458b590f1321f7cd8e94c55119
ansaryergesh <yergeshovansar@gmail.com>
2020-01-09 01:48:43 +0600

    helper function

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let scores = [0, 0];
 
 let winValues = [7,56,73,84,146,273,292,448];
 
+let gameOver = false;
+
 scores[0]
 let turnOf = 0;
 players[0] = "Player1";
@@ -11,32 +13,45 @@ players[1] = "Player2";
 
 //Build Board
 
-
+//helpers
+function updateGameMessage(message) {
+    document.getElementById("turn-text").innerText = message;
+}
 //Check if winning
 
 function winCheck() {
     for (var i = 0; i< winValues.length; i++){
         if((scores[turnOf] & winValues[i]) == winValues[i]){
-            alert(players[turnOf] + " Wins");
+            updateGameMessage(players[turnOf] + " Wins!");
+            gameOver = true;
         }
+
+    }
+
+    
+    if (((scores[0] + scores[1]) == 511) && !gameOver) {
+        updateGameMessage(" Draw!")
+        gameOver = true;
     }
 }
 //Play the game
 
 function playGame(clickDiv, boxValue) {
+    if (!gameOver) {
     scores[turnOf] += boxValue;
 
     clickDiv.onclick = "";
     clickDiv.innerHTML = "<span> "+ symbols[turnOf] +" </span>";
     winCheck();
-    switchTurn();
+    if (!gameOver) {switchTurn();}
+    }
 }
 
 function switchTurn() {
     if(turnOf == 0) turnOf = 1; 
     else turnOf = 0;
 
-    document.getElementById("turn-text").innerText = players[turnOf] + "'s turn "
+    updateGameMessage(players[turnOf] + "'s turn ");
 }
 //Keep track of whose turn
 
